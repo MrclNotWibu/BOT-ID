107fd34d2a0501a64f4cb89cde2876ab18d6d329
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 06:25:09 +0700

    Create mrcll-dana.js

---
+++ plugins/mrcll-dana.js
@@ -0,0 +1,38 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `${htki} DANA ${htka}
+
+Hay👋, ingin melanjutkan pembayaran?
+
+💰 *Pembayaran*
+• Via: Dana
+• Nomor: 081381685954
+• A/n: MrclL
+• Mitra: MrclNotWibu
+• Metode pembayaran: Online ( ~Cod~ )
+
+📦 _Informasi Pembayaran_
+
+Pembayaran Sewa hanya dapat menggunakan pulsa.
+Pastikan pulsa kamu mencukupi untuk bertransaksi!
+
+
+📮KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dana$/i
+handler.private = true
+
+export default handler
