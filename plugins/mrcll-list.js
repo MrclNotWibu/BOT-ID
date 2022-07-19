1ebd96a13a31b0565590a5d2b02c123794de9f16
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 10:22:20 +0700

    Update mrcll-list.js

--- plugins/mrcll-list.js
+++ plugins/mrcll-list.js
@@ -1,1 +1,44 @@
-P
+let handler = async m => m.reply(`
+「 MrclL Shop 」
+
+┏⫹⫺ *JASA CMD* 
+┗$ .jcmd
+
+┏⫹⫺ *ACHIEVEMENT MC* 
+┗$ .achievement 
+
+┏⫹⫺ *WEB ONLINE* 
+┗$ .webonline
+
+┏⫹⫺ *PAID EDIT* 
+┗$ .paidedit
+
+┏⫹⫺ *SEWABOT* 
+┗$ .sewabot
+
+┏⫹⫺ *JADIBOT* 
+┗$ .jbot
+
+*PAYMENT:*
+
+┏⫹⫺ *PULSA INDOSAT* 
+┗$ .pulsaindosat
+
+┏⫹⫺ *PULSA TELKOM* 
+┗$ .pulsatelkom
+
+┏⫹⫺ *OVO* 
+┗$ .ovo
+
+┏⫹⫺ *DANA* 
+┗$ .dana
+
+┏⫹⫺ *GOPAY* 
+┗$ .gopay
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['listshop']
+handler.tags = ['info']
+handler.command = /^listshop$/i
+
+module.exports = handler
+
