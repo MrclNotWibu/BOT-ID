af1be914a69ba16ccd9a186c83af0b92e4427d38
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 09:47:24 +0700

    Update mrcll-gopay.js

--- plugins/mrcll-gopay.js
+++ plugins/mrcll-gopay.js
@@ -1,10 +1,8 @@
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `${htki} GOPAY ${htka}
-
+let handler = async m => m.reply(`
 Hay👋, ingin melanjutkan pembayaran?
 
 💰 *Pembayaran*
-• Via: Gopay
+• Via: Ovo
 • Nomor: 081381685954
 • A/n: MrclL
 • Mitra: MrclNotWibu
@@ -12,27 +10,11 @@ Hay👋, ingin melanjutkan pembayaran?
 
 📦 _Informasi Pembayaran_
 
-Pembayaran Sewa hanya dapat menggunakan pulsa.
-Pastikan pulsa kamu mencukupi untuk bertransaksi!
-
-
-📮KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
-`
-const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Chat Admin', url: 'https://wa.me/+6281381685954?text=Bang+Mau+Selesaiin+Pembayaran'}},
-    {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
-]
-let tm = {
-text: text,
-footer: global.wm,
-templateButtons: templateButtons,
-image: {url: fla + 'Donasi'}
-}
-conn.sendMessage(m.chat, tm, m)
-}
+Pembayaran Sewa hanya dapat menggunakan ovo.
+Pastikan saldo kamu mencukupi untuk bertransaksi!
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
-handler.command = /^gopay$/i
-handler.private = true
+handler.command = /^dona(te|si)$/i
 
-export default handler
+module.exports = handler
