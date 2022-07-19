5baa717ccba9898af3bac27a666bb2e15a44db2d
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 12:33:36 +0700

    Update mrcll-jasacmd.js

--- plugins/mrcll-jasacmd.js
+++ plugins/mrcll-jasacmd.js
@@ -1,5 +1,35 @@
 let handler = async m => m.reply(`
-Coming soon sayang
+╭─「 List 」
+│ •  SCOREBOARD
+│ •  SISTEM KILL
+│ •  SISTEM JUMP (BUG)
+│ •  Sistem Bank Biasa
+│ •  Sistem Bank UI
+│ •  Sistem Rank Biasa
+│ •  Sistem Rank + Fiture
+│ •  Warp Book
+│ •  Warp UI
+│ •  Lobby Proctection
+│ •  Ban Item
+│ •  Lobby Particle
+│ •  Buy & Sell Biasa
+│ •  Buy & Sell UI
+╰────
+
+╭─「 Harga 」
+│ •  4K
+│ •  4K
+│ •  Bug
+│ •  25K
+│ •  45K
+│ •  10K
+│ •  25K
+│ •  1 Warp 2K
+│ •  1 Warp 4K
+│ •  1 TEMPAT 2K
+│ •  1 ITEM 1K
+│ •  1 TEMPAT 2K
+╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['jcmd']
 handler.tags = ['info']
