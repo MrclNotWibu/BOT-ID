afba2be8e022ea85d3b9967aa0c98752abacf3a3
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 09:26:25 +0700

    Create mrcll-shop1.js

---
+++ plugins/mrcll-shop1.js
@@ -0,0 +1,120 @@
+let handler = async m => m.reply(`
+
+*<====[ SEWA BOT ]====>*
+┏⫹⫺ *ʜᴇᴍᴀᴛ* 
+┗$ $ 5k/grup (1 minggu)
+
+┏⫹⫺ *ɴᴏʀᴍᴀʟ* 
+┗$ $ 10k/grup (1 bulan)
+
+┏⫹⫺ *sᴛᴀɴᴅᴀʀ* 
+┗$ $ 25k/grup (2 bulan)
+
+┏⫹⫺ *ᴘʀᴏ* 
+┗$ $ 30k/grup (3 bulan)
+
+┏⫹⫺ *ᴘᴇʀᴍᴀɴᴇɴ* 
+┗$ $ 40k/grup (Permanen)
+
+──···────────────────···──
+
+*<====[ ᴘʀᴇᴍɪᴜᴍ ]====>*
+┏⫹⫺ *ʜᴇᴍᴀᴛ* 
+┗$ $ 5k (1 minggu)
+
+┏⫹⫺ *ɴᴏʀᴍᴀʟ* 
+┗$ $ 10k (1 bulan)
+
+┏⫹⫺ *ɢᴏᴏᴅ* 
+┗$ $ 15k (3 bulan)
+
+┏⫹⫺ *ᴘʀᴏ* 
+┗$ $ 20k (5 bulan)                        
+
+──···────────────────···──
+
+*<====[ JADIBOT ]====>*
+┏⫹⫺ *JADIBOT NO OWN* 
+┗$ $ 15k (1 minggu)
+
+┏⫹⫺ *JADIBOT + OWN* 
+┗$ $ 25k (1 minggu)
+
+┏⫹⫺ *JADIBOT + OWN* 
+┗$ $ 35k (permanen)
+
+──···────────────────···──
+
+*<====[ JASA COMMANDER ]====>*
+┏⫹⫺ *SCOREBOARD* 
+┗$ $ 4K
+
+┏⫹⫺ *SISTEM KILL* 
+┗$ $ 4K
+
+┏⫹⫺ *SISTEM JUMP ( BUG )* 
+┗$ $ BUG
+
+┏⫹⫺ *SISTEM BANK BIASA* 
+┗$ $ 25K
+
+┏⫹⫺ *SISTEM BANK UI* 
+┗$ $ 45K
+
+┏⫹⫺ *SISTEM RANK BIASA* 
+┗$ $ 10K
+
+┏⫹⫺ *SISTEM RANK + FITURE* 
+┗$ $ 35K
+
+┏⫹⫺ *WARP BOOK* 
+┗$ $ 1 WARP 2K
+
+┏⫹⫺ *WARP UI* 
+┗$ $ 1 WARP 4K
+
+┏⫹⫺ *LOBBY PROCTECTION* 
+┗$ $ 1 TEMPAT 2K
+
+┏⫹⫺ *BAN ITEM* 
+┗$ $ 1 ITEM 1K
+
+┏⫹⫺ *LOBBY PARTICLE* 
+┗$ $ 1 TEMPAT 4K
+
+┏⫹⫺ *BUY SHOP & SELL SHOP BIASA* 
+┗$ $ 25K
+
+┏⫹⫺ *BUY SHOP & SELL SHOP UI* 
+┗$ $ 45K
+
+──···────────────────···──
+
+*<====[ PAYMENT ]====>*
+┏⫹⫺ *GOPAY* 
+┗$ .gopay
+
+┏⫹⫺ *DANA* 
+┗$ .dana
+
+┏⫹⫺ *OVO* 
+┗$ .ovo
+
+┏⫹⫺ *PULSA TELKOMSEL* 
+┗$ .telkomsel
+
+┏⫹⫺ *PULSA INDOSAT* 
+┗$ .indosat
+
+–––––– *ᴋᴇʙɪᴊᴀᴋᴀɴ* ––––––
+🗣️: Kak, Kok harganya mahal banget?
+💬: Mau tawar menawar? boleh, silahkan chat owner aja.. max tawar 5k
+
+🗣️: Scam ga nih kak?
+💬: Enggalah, Owner 100% Tepati janji #STAYHALAL
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['mrcllshop']
+handler.tags = ['info']
+handler.command = /^mrcllshop$/i
+
+module.exports = handler
