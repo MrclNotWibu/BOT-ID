a1faf59b136a3f4da63a8b66d315b063779381c3
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 14:16:40 +0700

    Update mrcll-payyy.js

--- plugins/mrcll-payyy.js
+++ plugins/mrcll-payyy.js
@@ -14,9 +14,11 @@ let handler = async m => m.reply(`
 
 ┏⫹⫺ *PULSA INDOSAT* 
 ┗$ .indosat
+
+*NOTE:* Jika sudah membayar chat https://wa.me/+6281381685954
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['pay']
-handler.tags = ['myshop']
+handler.tags = ['info']
 handler.command = /^pay$/i
 
 module.exports = handler
