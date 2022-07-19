db910996735d5957b3693c882179399058fd794a
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 12:56:41 +0700

    Update mrcll-paidedit.js

--- plugins/mrcll-paidedit.js
+++ plugins/mrcll-paidedit.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 Coming soon sayang
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['paidedit']
-handler.tags = ['info']
+handler.tags = ['myshop']
 handler.command = /^paidedit$/i
 
 module.exports = handler
