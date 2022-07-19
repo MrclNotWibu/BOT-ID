560133ac5434403d188225cb1c67731635cae45c
MrclNotWibu <108334344+MrclNotWibu@users.noreply.github.com>
2022-07-19 14:10:52 +0700

    Update mrcll-jadibot.js

--- plugins/mrcll-jadibot.js
+++ plugins/mrcll-jadibot.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 Ketik .sewabot
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['jbot']
-handler.tags = ['myshop']
+handler.tags = ['info']
 handler.command = /^jbot$/i
 
 module.exports = handler
